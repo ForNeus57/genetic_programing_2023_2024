32f99d1fc19a9081808eafce1e9aac9db791e33e
ForNeus57 <dominikbreksa@gmail.com>
2023-12-11 16:12:37 +0100

    Simplifer done

--- grammar/src/simplifier/simplifier.py
+++ grammar/src/simplifier/simplifier.py
@@ -212,46 +212,45 @@ def cut_program_from_file(file_path: str) -> str:
 
 
 if __name__ == '__main__':
-    # program: str = cut_program_from_file(argv[1])
-    # simplifier: Simplifier = Simplifier(program)
+    program: str = cut_program_from_file(argv[1])
+    simplifier: Simplifier = Simplifier(program)
 
     # simplifier: Simplifier = Simplifier("((1 * (3.2 + 3.4)) * 3)")
     # print(simplifier.simplify())
-    #
-    simplifier: Simplifier = Simplifier('(((cos((sin((0.15792111801538056 / ((sin((((cos(4.390922989460659)) - (cos((sin(((3.9378125013554417 - (4.567186184882816 + (4.9798113857157915 - (2.879804272999489 + 1.8159963035888813)))) * 1.636289987009465)))))) - 4.167758199134466))) - 2.0531718571910895)))))) / 2.637638162486309) + (1.6247319950636392 + (X1 * X1)))')
-    print(simplifier.simplify())
+
+    # simplifier: Simplifier = Simplifier('(((cos((sin((0.15792111801538056 / ((sin((((cos(4.390922989460659)) - (cos((sin(((3.9378125013554417 - (4.567186184882816 + (4.9798113857157915 - (2.879804272999489 + 1.8159963035888813)))) * 1.636289987009465)))))) - 4.167758199134466))) - 2.0531718571910895)))))) / 2.637638162486309) + (1.6247319950636392 + (X1 * X1)))')
+    # print(simplifier.simplify())
 
     # simplifier: Simplifier = Simplifier('(0.3752679242505537 + (1.6247319950636392 + (X1 * X1)))')
     # print(simplifier.simplify())
 
-
-# simplifier: Simplifier = Simplifier("(1 * ((( 3.2 + 3.4) / ( 3.3 + 3.3)) * 2))")
+    # simplifier: Simplifier = Simplifier("(1 * ((( 3.2 + 3.4) / ( 3.3 + 3.3)) * 2))")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("(((sin(1)) + 1))")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("(((sin(1)) + X1))")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("((X1 * ( 3.2 + 3.4)) * 2)")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("((( 3.2 + 3.4) * X1) * 2)")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("((( 3.2 + 3.4) + X1) + 2)")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("(2 + (( 3.2 + 3.4) + X1))")
     # print(simplifier.simplify())
-    #
+
     # simplifier: Simplifier = Simplifier("(2 + (X1 + ( 3.2 + 3.4)))")
     # print(simplifier.simplify())
-    #
-    #
+
+
     # simplifier: Simplifier = Simplifier("(X1  * ((( 3.2 + 3.4) / ( 3.3 + 3.3)) * 2))")
     # print(simplifier.simplify())
 
-    # with open(argv[2], 'w') as output_file:
-    #     output_file.write(simplifier.simplify())
+    with open(argv[2], 'w') as output_file:
+        output_file.write(simplifier.simplify())
