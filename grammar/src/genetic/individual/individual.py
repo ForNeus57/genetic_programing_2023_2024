2db9e75a16f2defef5b7a292c132009c71af62bc
ForNeus57 <dominikbreksa@gmail.com>
2024-01-09 00:32:03 +0100

    Added tournament and evaluation

--- grammar/src/genetic/individual/individual.py
+++ grammar/src/genetic/individual/individual.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from pickle import dump, load
+from typing import Callable, Union, Literal
 
 from genetic.individual.structure.rules import Program
 
@@ -22,11 +23,15 @@ class Individual:
     def __str__(self) -> str:
         return str(self.program)
 
-    def execute(self, *args, **kwargs) -> tuple:
+    def execute(self, input_vector: tuple) -> tuple:
         raise NotImplementedError()
 
-    def evaluate(self, *args, **kwargs) -> float:
-        raise NotImplementedError()
+    def evaluate(self, fitness_function: Callable[[tuple, tuple],  Union[float, int, bool]],
+                 input_vector: tuple,
+                 model_vector: tuple) -> Union[float, int, bool]:
+        result_vector: tuple = self.execute(input_vector)
+
+        return fitness_function(result_vector, model_vector)
 
     def mutate(self) -> Individual:
         raise NotImplementedError()
@@ -37,3 +42,16 @@ class Individual:
     def save_to_file(self, path: str):
         with open(path, 'wb') as file:
             dump(self, file)
+
+    @staticmethod
+    def tournament(individuals: tuple[Individual, ...],
+                   fitness_function: Callable[[tuple, tuple],  Union[float, int, bool]],
+                   input_vector: tuple,
+                   model_vector: tuple,
+                   mode: Literal['min', 'max']) -> Individual:
+        if mode == 'min':
+            return min(individuals, key=lambda individual: individual.evaluate(fitness_function, input_vector, model_vector))
+        elif mode == 'max':
+            return max(individuals, key=lambda individual: individual.evaluate(fitness_function, input_vector, model_vector))
+
+        raise ValueError(f'Unknown mode: {mode}')
