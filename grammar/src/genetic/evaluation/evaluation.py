2aade05a2fe67505ef0bc10c2ce7348e380fd5b1
Kaja Dzielnicka <kajkad2002@gmail.com>
2024-02-01 21:46:57 +0100

    evaluuuation.py refactor

--- grammar/src/genetic/evaluation/evaluation.py
+++ grammar/src/genetic/evaluation/evaluation.py
@@ -1,109 +1,83 @@
-from typing import List, Union, Optional
+from typing import Tuple, Union, Optional, Callable
 
-def convert_output(gp_output: List[Union[int, float, bool]]) -> List[float]:
-    """
-    Function converts output of genetic program to list of floats.
-    Boolean values are converted to 0.0 and 1.0.
-    """
-    return [float(x) if not isinstance(x, bool) else float(int(x)) for x in gp_output]
+from typing import Tuple, Union, Optional, Callable
 
-def evaluate_gp_performance(gp_output: Union[List[int], List[float], List[bool]], test_case: str, gp_input: Optional[List[int]] = None) -> float:
-    """
-    Function evaluates performance of genetic program.
+def convert_output(gp_output: Union[Tuple[int, ...], Tuple[float, ...], Tuple[bool, ...]]) -> Tuple[int, ...]:
+    return tuple([int(x) if not isinstance(x, bool) else int(x) for x in gp_output])
 
-    Args:
-        gp_output: Output of genetic program.
-        test_case: Test case to evaluate.
-        gp_input: Input of genetic program.
-
-    Returns:
-        The evaluation result as a non-negative integer or float.
-    """
-    gp_output = convert_output(gp_output)
-    fitness = 0
-
-    if test_case.startswith("1.1"):
-        fitness = evaluate_scenario_1_1(gp_output, test_case)
-    elif test_case.startswith("1.2"):
-        fitness = evaluate_scenario_1_2(gp_output, test_case, gp_input)
-    elif test_case.startswith("1.3"):
-        fitness = evaluate_scenario_1_3(gp_output, test_case, gp_input)
-    elif test_case.startswith("1.4"):
-        fitness = evaluate_scenario_1_4(gp_output, test_case, gp_input)
-    return fitness
-
-def evaluate_scenario_1_1(gp_output: List[float], test_case: str) -> float:
-    fitness = 0
-
-    if test_case in ["1.1.A", "1.1.B", "1.1.C"]:
-        target_value = {
-            "1.1.A": 1,
-            "1.1.B": 789,
-            "1.1.C": 31415
-        }[test_case]
-        closest_value = min(gp_output, key=lambda x: abs(x - target_value))
-        fitness += abs(closest_value - target_value) if target_value not in gp_output else 0
-    elif test_case in ["1.1.D", "1.1.E"]:
-        first_position_value = {
-            "1.1.D": 1,
-            "1.1.E": 789
-        }[test_case]
-        fitness += abs(gp_output[0] - first_position_value) if gp_output[0] != first_position_value else 0
-    elif test_case == "1.1.F":
-        if len(gp_output) == 1:
-            fitness += abs(gp_output[0] - 1) if gp_output[0] != 1 else 0
+def create_fitness_function_for_1_1(target_value: int, check_first_only: bool, single_output_only: bool) -> Callable[[Tuple[int, ...]], int]:
+    def fitness_function(gp_output: Tuple[int, ...]) -> int:
+        if single_output_only and len(gp_output) != 1:
+            return 9999999
+        if check_first_only:
+            return abs(gp_output[0] - target_value) if gp_output[0] != target_value else 0
         else:
-            fitness += 9999999
+            closest_value = min(gp_output, key=lambda x: abs(x - target_value))
+            return abs(closest_value - target_value) if target_value not in gp_output else 0
+    return fitness_function
 
-    return fitness
+def create_fitness_function_for_numerical_operations(operation: Callable[[Tuple[int, ...]], int]) -> Callable[[Tuple[int, ...], Tuple[int, ...]], int]:
+    def fitness_function(gp_output: Tuple[int, ...], gp_input: Tuple[int, ...]) -> int:
+        if len(gp_output) != 1:
+            return 9999999
+        expected_result = operation(gp_input)
+        return abs(gp_output[0] - expected_result)
+    return fitness_function
 
-def evaluate_scenario_1_2(gp_output: List[float], test_case: str, gp_input: List[int]) -> float:
-    fitness = 0
+def create_fitness_function_for_vector_operations(operation: Callable[[Tuple[int, ...]], Tuple[int, ...]]) -> Callable[[Tuple[int, ...], Tuple[int, ...]], int]:
+    def fitness_function(gp_output: Tuple[int, ...], gp_input: Tuple[int, ...]) -> int:
+        expected_result = operation(gp_input)
+        if len(gp_output) != len(expected_result):
+            return 9999999
+        return sum(abs(o - e) for o, e in zip(gp_output, expected_result))
+    return fitness_function
 
-    expected_result = None
-    if test_case in ["1.2.A", "1.2.B", "1.2.C"]:
-        expected_result = sum(gp_input[:2])
-    elif test_case == "1.2.D":
-        expected_result = gp_input[0] - gp_input[1]
-    elif test_case == "1.2.E":
-        expected_result = gp_input[0] * gp_input[1]
+def sum_operation(gp_input: Tuple[int, ...]) -> int:
+    return sum(gp_input[:2])
 
-    actual_result = gp_output[0]
-    if len(gp_output) != 1:
-        fitness += 9999999
-    else:
-        fitness += abs(actual_result - expected_result) if actual_result != expected_result else 0
+def diff_operation(gp_input: Tuple[int, ...]) -> int:
+    return gp_input[0] - gp_input[1]
 
-    return fitness
+def mul_operation(gp_input: Tuple[int, ...]) -> int:
+    return gp_input[0] * gp_input[1]
 
-def evaluate_scenario_1_3(gp_output: List[float], test_case: str, gp_input: List[int]) -> float:
-    fitness = 0
+def max_operation(gp_input: Tuple[int, ...]) -> int:
+    return max(gp_input[:2])
 
-    expected_result = None
-    if test_case in ["1.3.A", "1.3.B"]:
-        expected_result = max(gp_input[:2])
+def average_operation(include_first: bool, gp_input: Tuple[int, ...]) -> int:
+    inputs = gp_input[:10] if include_first else gp_input[1:gp_input[0] + 1]
+    return int(sum(inputs) / len(inputs))
 
-    actual_result = gp_output[0]
-    if len(gp_output) != 1:
-        fitness += 9999999
-    else:
-        fitness += abs(actual_result - expected_result) if actual_result != expected_result else 0
+def sum_int_and_simulated_float(gp_input: Tuple[int, ...]) -> float:
+    float_to_add = gp_input[1] + (gp_input[2] / 10 ** len(str(gp_input[2])))
+    return gp_input[0] + float_to_add
 
-    return fitness
+def negative_to_zero_operation(gp_input: Tuple[int, ...]) -> Tuple[int, ...]:
+    return tuple(x if x >= 0 else 0 for x in gp_input)
 
-def evaluate_scenario_1_4(gp_output: List[float], test_case: str, gp_input: List[int]) -> float:
-    fitness = 0
+def smallest_operation(gp_input: Tuple[int, ...]) -> int:
+    if len(gp_input) != 4:
+        return 9999999
+    return min(gp_input)
 
-    expected_result = None
-    if test_case == "1.4.A":
-        expected_result = sum(gp_input[:10]) / 10
-    elif test_case == "1.4.B":
-        expected_result = sum(gp_input[1:gp_input[0] + 1]) / gp_input[0]
 
-    actual_result = gp_output[0]
-    if len(gp_output) != 1:
-        fitness += 9999999
-    else:
-        fitness += abs(actual_result - expected_result) if actual_result != expected_result else 0
-
-    return fitness
+fitness_functions = {
+    "1.1.A": create_fitness_function_for_1_1(1, False, False),
+    "1.1.B": create_fitness_function_for_1_1(789, False, False),
+    "1.1.C": create_fitness_function_for_1_1(31415, False, False),
+    "1.1.D": create_fitness_function_for_1_1(1, True, False),
+    "1.1.E": create_fitness_function_for_1_1(789, True, False),
+    "1.1.F": create_fitness_function_for_1_1(1, False, True),
+    "1.2.A": create_fitness_function_for_numerical_operations(sum_operation),
+    "1.2.B": create_fitness_function_for_numerical_operations(sum_operation),
+    "1.2.C": create_fitness_function_for_numerical_operations(sum_operation),
+    "1.2.D": create_fitness_function_for_numerical_operations(diff_operation),
+    "1.2.E": create_fitness_function_for_numerical_operations(mul_operation),
+    "1.3.A": create_fitness_function_for_numerical_operations(max_operation),
+    "1.3.B": create_fitness_function_for_numerical_operations(max_operation),
+    "1.4.A": create_fitness_function_for_numerical_operations(lambda x: average_operation(False, x)),
+    "1.4.B": create_fitness_function_for_numerical_operations(lambda x: average_operation(True, x)),
+    "B.1": create_fitness_function_for_numerical_operations(sum_int_and_simulated_float),
+    "B.15": create_fitness_function_for_vector_operations(negative_to_zero_operation),
+    "B.28": create_fitness_function_for_numerical_operations(smallest_operation)
+}
