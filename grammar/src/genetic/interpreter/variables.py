84f3dad61c8f1b54c3d053836381ec3074d9d2bc
ForNeus57 <dominikbreksa@gmail.com>
2024-02-01 11:46:54 +0100

    Simplified variables.py by removing extra None possibility.

--- grammar/src/genetic/interpreter/variables.py
+++ grammar/src/genetic/interpreter/variables.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Literal, Union
 
 
 @dataclass(slots=True)
 class Variable:
     type: Literal['int', 'bool']
-    value: Union[int, bool, None] = field(default=None)
+    value: Union[int, bool]
